cfd5868327f533ec41b9947770555335dd196dce
woaimi520 <woaimi520@163.com>
2019-06-18 20:15:25 +0800

    5

---
+++ 签名查看.bat
@@ -0,0 +1,8 @@
+
+@echo off
+set filename=%~n1%~x1
+cd %~dp1
+echo 以下是%filename%的签名信息：
+echo ----------------------------------------------------------
+keytool -list -printcert -jarfile "%filename%"
+pause
